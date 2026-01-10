24c0cafe43a964304a2c6c6e5b7174f267de76e1
Ravikumar Karunakaran <ravikumarsoftware18@gmail.com>
2026-01-10 17:55:17 +0530

    Fix Next.js export build error

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   images: {
-    unoptimized: true, // REQUIRED for static export
+    unoptimized: true,
   },
 };
 
